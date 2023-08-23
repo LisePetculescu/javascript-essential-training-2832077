3fca3056df9129f7be8c17adc667a23c309be352
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-08-23 18:08:46 +0200

    Update script.js

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,21 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const myPurse = {
+    color: "white, with flowers",
+    heightInCm: 30,
+    widthInCm: 28,
+    zipper: true,
+    strapLengthCm: 50,
+    pockets: 2,
+    contents: {
+        phone: true,
+        keys: true,
+        lipstick: true,
+        lipstickColour: "pink",
+        wallet: false
+    }
+}
+
